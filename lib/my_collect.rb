858dc437a8e19cdfd632b1e71e065be209672017
Omar Branez <omar.branez@gmail.com>
2021-02-06 17:54:39 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,4 +8,12 @@ def my_collect(array)
     collection
 end
         
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# my_collect(array) do |name|
+#   name.split(" ").first
+# end 
 
+# collection = ['ruby', 'javascript', 'python', 'objective-c']
+# my_collect(collection) do |lang|
+#   lang.upcase
+# end
